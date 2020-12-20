ecd2d85a939b4bf960a89503e0a106e40324bad4
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-20 14:33:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,31 +6,40 @@ end
 addition(5,4)
 
 def subtraction(num1, num2)
-result=
-puts (num1-num2)
+result=(num1-num2)
+puts result
+result
 end
 subtraction(5,4)
 
 
 
 def division(num1,num2)
-puts (num1.to_f/num2.to_f)
+result=(num1.to_f/num2.to_f)
+puts result
+result
 end
 division(34,5)
 
 
 def multiplication(num1, num2)
-puts (num1.to_f*num2.to_f)
+result=(num1.to_f*num2.to_f)
+puts result
+result
 end
 multiplication(5,4)
 
 def modulo(num1, num2)
-puts(num1%num2)
+result=(num1%num2)
+puts result
+result
 end
 modulo(34,5)
 
 
 def square_root(num)
-puts Math.sqrt (num)
+result=Math.sqrt (num)
+puts result
+result
 end
 square_root(81)
