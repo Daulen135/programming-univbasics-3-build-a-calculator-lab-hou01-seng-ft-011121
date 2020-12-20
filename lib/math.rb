226b5924aff77c7dc89ecacc1db8eb7d72aa6242
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-20 13:19:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,19 @@
 def addition(num1, num2)
-
+puts (num1.to_f+ num2.to_f)
 end
+addition(5,4)
 
 def subtraction(num1, num2)
-
+puts (num1-num2)
 end
+subtraction(5,4)
 
-def division(num1, num2)
 
+
+def division(num1,num2)
+puts (num1.to_f/num2.to_f)
 end
+division(5,4)
 
 def multiplication(num1, num2)
 
